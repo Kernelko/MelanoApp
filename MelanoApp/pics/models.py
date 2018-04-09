99a809912287121971bbd7503c71eb24794e6d23
Kernelko <zuzanna.onderko@gmail.com>
2018-04-09 12:41:59 +0200

    adding static

--- MelanoApp/pics/models.py
+++ MelanoApp/pics/models.py
@@ -16,7 +16,7 @@ class Picture_file(models.Model):
     file_type = models.CharField(max_length=10)
     picture_date= models.DateField()
     #picture = models.ImageField()
-    is_cancer = models.BooleanField(default=True)
+    is_cancer = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.picture_date)
