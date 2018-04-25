2805ba524b434209174484b2ffa023c09ec06c5b
Kernelko <zuzanna.onderko@gmail.com>
2018-04-25 22:19:48 +0200

    urls updated

--- MelanoApp/pics/urls.py
+++ MelanoApp/pics/urls.py
@@ -14,10 +14,10 @@ urlpatterns = [
     url(r'mark/add/$', views.MarkCreate.as_view(), name = "mark-add" ),
 
     #pics/mark/2/
-    url(r'mark/^(?P<pk>[0-9]+)/$', views.MarkCreate.as_view(), name = "mark-add" ),
+    url(r'mark/^(?P<pk>[0-9]+)/$', views.MarkUpdate.as_view(), name = "mark-update" ),
 
-    #pics/mark/add/
-    url(r'mark/add/$', views.MarkCreate.as_view(), name = "mark-add" ),
+    #pics/mark/2/delete
+    url(r'mark/^(?P<pk>[0-9]+)/delete/$', views.MarkDelete.as_view(), name = "mark-delete" ),
 
     
 ]
